a7da4ce7e2928b9bd8e22f0e5a7405b5a302e4bb
Chris John Robbertse <conzenite17@gmail.com>
2018-09-19 23:03:06 +0100

    created base hamburger class with some variables

---
+++ src/Hamburger.java
@@ -0,0 +1,7 @@
+public class Hamburger {
+    private String breadType;
+    private String meat;
+    private String[] additions;
+
+    //potentially create objects for the ingredients??? think about it :)
+}
